2e9e85dea93c5fd66b147b72ba642108972fb0f0
EC2 Default User <ec2-user@ip-172-31-38-28.ec2.internal>
2026-07-14 20:05:25 +0000

    Add console output for application

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Calculator;
+
+MathOperations math = new();
+
+Console.WriteLine("===== .NET Application Started =====");
+Console.WriteLine($"5 + 10 = {math.Add(5, 10)}");
+Console.WriteLine($"5 x 10 = {math.Multiply(5, 10)}");
+Console.WriteLine("===== Application Completed =====");// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
